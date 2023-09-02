3f512ecaa94662a4d64bb7d4a49f7e6be81f8fe6
Maxim Karachyov <karachyov.m@gmail.com>
2023-09-02 21:38:32 +0600

    tailwind

---
+++ src/components/TaskList.vue
@@ -0,0 +1,49 @@
+<template>
+	<ol class="task">
+		<li class="task__item" v-for="item in list" v-bind:key="item.id">
+			<div class="task__name">{{ item.name }}</div>
+			<div class="task__del">x</div>
+		</li>
+	</ol>
+</template>
+
+<script>
+import {onMounted} from 'vue'
+import {initFlowbite} from 'flowbite'
+
+export default {
+	name: "TaskList",
+	props: {
+		list: {
+			default: () => [],
+			type: Array,
+		}
+	},
+	setup() {
+		onMounted(() => {
+			initFlowbite();
+		})
+	}
+}
+</script>
+
+<style scoped lang="scss">
+.task {
+	margin: 0 auto;
+	padding: 0;
+	max-width: 400px;
+
+	&__item {
+		display: flex;
+		gap: 20px;
+	}
+
+	&__name {
+
+	}
+
+	&__del {
+
+	}
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="home">
+		<TaskAdd></TaskAdd>
+		<TaskList v-bind:list="ArrList"></TaskList>
+	</div>
+</template>
+
+<script>
+
+import TaskList from "@/components/TaskList";
+import TaskAdd from "@/components/TaskAdd";
+
+export default {
+	name: 'HomeView',
+	components: {TaskAdd, TaskList},
+	data: function () {
+		return {
+			ArrList: [
+				{id: 1, name: 'Задача 1'},
+				{id: 2, name: 'Задача 2'},
+				{id: 3, name: 'Задача 3'},
+				{id: 4, name: 'Задача 4'},
+			]
+		}
+	},
+	setup() {
+
+
+		return {}
+	}
+}
+</script>
